428890de93efe86185d259f3a1d183b5dbb637e8
Angga Z Situmorang <anggazefanya6@gmail.com>
2023-11-17 11:57:12 +0700

    ini Buttom_Navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.kerjaparaktik"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.kerjaparaktik"
