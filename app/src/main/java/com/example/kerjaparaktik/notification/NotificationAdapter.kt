0124b060a8629772ceecf15ee0b4a72b3c3ff784
Angga Z Situmorang <anggazefanya6@gmail.com>
2023-11-18 13:55:02 +0700

    Notification Page

---
+++ app/src/main/java/com/example/kerjaparaktik/notification/NotificationAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.kerjaparaktik.notification
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kerjaparaktik.R
+
+class NotificationAdapter (private var mList: List<NotificationData>) :
+    RecyclerView.Adapter<NotificationAdapter.LanguageViewHolder>() {
+
+    inner class LanguageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val title : TextView = itemView.findViewById(R.id.textNotif)
+        val image : ImageView = itemView.findViewById(R.id.gambarNotif)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):LanguageViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_notification, parent , false)
+        return LanguageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NotificationAdapter.LanguageViewHolder, position: Int) {
+        holder.image.setImageResource(mList[position].image)
+        holder.title.text = mList[position].title
+    }
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+}
---
+++ app/src/main/java/com/example/kerjaparaktik/notification/NotificationData.kt
@@ -0,0 +1,3 @@
+package com.example.kerjaparaktik.notification
+
+data class NotificationData(var title: String, var image: Int)
