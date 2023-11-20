1548759ce195136a8286958cf3c0dbbbc9462a9f
Tria Ananda Suryani <trianandasy@gmail.com>
2023-11-20 21:54:07 +0700

    Landing page baru sign in google

--- app/src/main/java/com/example/kerjaparaktik/Login.kt
+++ app/src/main/java/com/example/kerjaparaktik/Login.kt
@@ -1,11 +1,18 @@
 package com.example.kerjaparaktik
 
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 
 class Login : AppCompatActivity() {
+    private lateinit var btnSignIn : ImageButton
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +23,11 @@ class Login : AppCompatActivity() {
         val txt_akunbaru: TextView = findViewById(R.id.txt_akunbaru)
         val txt_lupakatasandi: TextView = findViewById(R.id.txt_lupakatasandi)
 
+        btnSignIn = findViewById(R.id.btn_signin)
+        btnSignIn.setOnClickListener {
+            displayPopupDialog()
+        }
+
 
         txt_akunbaru.setOnClickListener {
             val intent = Intent(this@Login, Register::class.java)
@@ -25,6 +37,21 @@ class Login : AppCompatActivity() {
             val intent = Intent(this@Login, ForgotPass::class.java)
             startActivity(intent)
         }
+    }
+    private fun displayPopupDialog() {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.popup_signingoogle)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
+        val btnKonfirmasi = dialog.findViewById<Button>(R.id.btn_konfirmasi)
+        btnKonfirmasi.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.show()
+        btnKonfirmasi.setOnClickListener {
+            val intent = Intent(this@Login, Register::class.java)
+            startActivity(intent)
+        }
     }
 }
