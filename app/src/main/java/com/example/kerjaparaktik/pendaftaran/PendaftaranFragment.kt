7bcf846a34d8fa12d77acf30db1311beb2fc8a28
Angga Z Situmorang <anggazefanya6@gmail.com>
2023-11-30 16:40:23 +0700

    Upload image pendaftaran

--- app/src/main/java/com/example/kerjaparaktik/pendaftaran/PendaftaranFragment.kt
+++ app/src/main/java/com/example/kerjaparaktik/pendaftaran/PendaftaranFragment.kt
@@ -1,23 +1,20 @@
 package com.example.kerjaparaktik.pendaftaran
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Adapter
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
-import android.widget.Button
-import android.widget.Spinner
-import android.widget.Toast
-import com.example.kerjaparaktik.MainActivity
-import com.example.kerjaparaktik.R
+import android.widget.*
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
-
+import com.example.kerjaparaktik.R
+import com.google.android.gms.cast.framework.media.ImagePicker
 
 class PendaftaranFragment : Fragment() {
+    private lateinit var imageView: ImageView
+    private lateinit var button: Button
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,31 +22,42 @@ class PendaftaranFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pendaftaran, container, false)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        imageView = view.findViewById(R.id.gambarKtp)
+        button = view.findViewById(R.id.btnktp)
+
+        button.setOnClickListener {
+            com.github.dhaval2404.imagepicker.ImagePicker.with(this)
+                .crop()
+                .compress(1024)
+                .maxResultSize(1080, 1080)
+                .start()
+
+        }
+
         val autoMagang: AutoCompleteTextView = view.findViewById(R.id.magang)
         val autoPosisi: AutoCompleteTextView = view.findViewById(R.id.posisi)
-        val btnSubmit : Button=view.findViewById(R.id.btnsubmit)
+        val btnSubmit: Button = view.findViewById(R.id.btnsubmit)
 
-        val maganglist = listOf("PKL","Kerja Praktik")
+        val maganglist = listOf("PKL", "Kerja Praktik")
         val posisilist = listOf("Cyber Security")
 
-        val adapter1 = ArrayAdapter(requireContext(),R.layout.magang,maganglist)
-        val adapter2 = ArrayAdapter(requireContext(),R.layout.magang,posisilist)
+        val adapter1 = ArrayAdapter(requireContext(), R.layout.magang, maganglist)
+        val adapter2 = ArrayAdapter(requireContext(), R.layout.magang, posisilist)
 
         // Adapter Untuk Drop Down Menu
         autoMagang.setAdapter(adapter1)
-        autoMagang.onItemClickListener = AdapterView.OnItemClickListener {
-                adapterView, view, i, l ->
-            val itemSelected = adapterView.getItemAtPosition(i)
+        autoMagang.onItemClickListener = AdapterView.OnItemClickListener { _, _, i, _ ->
+            val itemSelected = adapter1.getItem(i)
             Toast.makeText(requireContext(), "$itemSelected", Toast.LENGTH_SHORT).show()
         }
 
         autoPosisi.setAdapter(adapter2)
-        autoPosisi.onItemClickListener = AdapterView.OnItemClickListener {
-                adapterView, view, b, l ->
-            val itemSelected2 = adapterView.getItemAtPosition(b)
+        autoPosisi.onItemClickListener = AdapterView.OnItemClickListener { _, _, b, _ ->
+            val itemSelected2 = adapter2.getItem(b)
             Toast.makeText(requireContext(), "$itemSelected2", Toast.LENGTH_SHORT).show()
         }
 
@@ -67,25 +75,10 @@ class PendaftaranFragment : Fragment() {
             // Lakukan transaksi
             transaction.commit()
         }
+    }
 
-//        // Adapter untuk Spinner
-//        val adapter = ArrayAdapter.createFromResource(
-//            requireContext(),
-//            R.array.magang_options,
-//            android.R.layout.simple_spinner_item
-//        )
-//
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-//
-//        // Set adapter ke Spinner
-//        spinnerMagang.adapter = adapter
-//
-//        val adapterPosisi = ArrayAdapter.createFromResource(
-//            requireContext(),
-//            R.array.posisi_options,
-//            android.R.layout.simple_spinner_item
-//        )
-//        adapterPosisi.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-//        spinnerPosisi.adapter = adapterPosisi
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        imageView.setImageURI(data?.data)
     }
 }
