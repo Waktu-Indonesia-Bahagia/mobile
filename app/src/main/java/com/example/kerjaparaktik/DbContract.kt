4413e35993b621cd90583e629bd62b3b8360b43f
Angga Z Situmorang <anggazefanya6@gmail.com>
2023-12-03 14:32:36 +0700

    update

--- app/src/main/java/com/example/kerjaparaktik/DbContract.kt
+++ app/src/main/java/com/example/kerjaparaktik/DbContract.kt
@@ -1,8 +1,7 @@
 package com.example.kerjaparaktik
 
 object DbContract {
-    const val ip = "192.168.123.74"
-
-    const val urlRegister = "http://$ip//kerjapraktik/api-register.php"
-    const val urlLogin = "http://$ip//kerjapraktik/api-login.php"
+    const val ip = "192.168.71.74"
+    const val urlRegister = "http://$ip//kerjapraktik_goldfinewe/api-register.php"
+    const val urlLogin = "http://$ip//kerjapraktik_goldfinewe/api-login.php"
 }
