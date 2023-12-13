f5315c4bd8b3ff104383c0c29d9a03dc15bcc33c
Syahrul ghifari <syahrulghifari47@gmail.com>
2023-12-13 08:04:12 +0700

    urledit

--- app/src/main/java/com/example/kerjaparaktik/DbContract.kt
+++ app/src/main/java/com/example/kerjaparaktik/DbContract.kt
@@ -1,7 +1,8 @@
 package com.example.kerjaparaktik
 
 object DbContract {
-    const val ip = "192.168.100.222"
+    const val ip = " 192.168.33.107"
     const val urlRegister = "http://$ip//app_massive/api-register.php"
     const val urlLogin = "http://$ip//app_massive/api-login.php"
+    const val urlEdit = "http://$ip//app_massive/api-edit.php"
 }
